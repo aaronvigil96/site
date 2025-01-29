bdeca2576654a1ade53cb9efc8de6a9467703f98
aaronvigil96 <aaronvigil96@gmail.com>
2025-01-29 13:19:57 -0300

    asdasd

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../stores/auth.store';
 
 const authApi = axios.create({
-    baseURL: "https://149.50.143.38:3000/api"
+    baseURL: "https://aaronvigil.com.ar/api/"
 })
 
 authApi.interceptors.request.use((config) => {
